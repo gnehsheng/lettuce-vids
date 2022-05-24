991ba61bc9fc9ee80dc8360e95d0e1c17e2d3ee3
NG YONG SHENG <gnehsheng@gmail.com>
2022-05-25 01:42:28 +0800

    typo in constants file

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -21,5 +21,5 @@ export const toastMsg = (description, status) => ({
 export const devFrontendUrl = "http://localhost:3000/";
 export const devBackendUrl = "http://localhost:8000/";
 
-export const prodFrontendUrl = "https://lettuce-vids.netlify.app";
-export const prodBackendUrl = "https://lettuce-backend.herokuapp.com";
+export const prodFrontendUrl = "https://lettuce-vids.netlify.app/";
+export const prodBackendUrl = "https://lettuce-backend.herokuapp.com/";
