df3430573bcfdc1ae3546daa2e21883c30515b10
NG YONG SHENG <gnehsheng@gmail.com>
2022-05-25 11:20:18 +0800

    changing urls for backend

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,4 +1,5 @@
 const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+const BACKEND_APP = process.env.BACKEND_APP
 
 export const getVideosInfoURL = (searchInput) =>
   `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=24&q=${searchInput}&type=video&key=${YOUTUBE_API_KEY}`;
@@ -22,4 +23,4 @@ export const devFrontendUrl = "http://localhost:3000/";
 export const devBackendUrl = "http://localhost:8000/";
 
 export const prodFrontendUrl = "https://lettuce-vids.netlify.app/";
-export const prodBackendUrl = "https://lettuce-backend.herokuapp.com/";
+export const prodBackendUrl = BACKEND_APP;
